bea8d2a6f4ad64b404a7d389911fd49b0727635a
javedkhan2k2 <javed_alam@outlook.com>
2023-07-02 00:13:04 +0200

    updated ex02

--- CPC07/ex02/ft_ultimate_range.c
+++ CPC07/ex02/ft_ultimate_range.c
@@ -13,15 +13,21 @@ int	ft_ultimate_range(int **range, int min, int max)
 	}
 
 	len = max - min;
-	range = (int**) malloc(len*sizeof(int));
-	if(!range)
+	//printf("Len:%d\n", len);
+	*range = (int*) malloc(len*sizeof(int));
+
+	if(!*range)
 		return (-1);
 	start = 0;
+	//printf("Len:%p\n", *range);
 	while(start < len)
 	{
-		*range[start] = min;
+		//printf("%d,", start);
+		//(*range)[start] = min;
+		*(*range+start) = min;
 		start++;
 		min++;
+		//**range = **range + 1;
 	}
 	return (len);
 
@@ -29,28 +35,12 @@ int	ft_ultimate_range(int **range, int min, int max)
 
 int main(void)
 {
-	int x[3][5]={{1,2,10,4,5},{6,7,1,9,10},{11,12,13,14,15}};
-	// *x = address of first byte
-	//**x:1, **(x+1):6, **(x+2):11
-
-
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-			printf("%d, ",x[i][j]);
-		}
-		printf("\n");
-		for(int j=0;j<5;j++)
-		{
-			printf("%d, ", **x(i+j));
-		}
-		printf("\n");
-	}
-	// int **ptr = NULL;
-	// int size = ft_ultimate_range(ptr, 1, 10);
-	// for(int i=0;i<size;i++)
-	// 	printf("%d, ", *ptr[i]);
-	// printf("\n");
+	int *ptr;
+	int res = ft_ultimate_range(&ptr, -5, 5);
+	if(res == -1)
+		return (0);
+	for(int i=0;i<res;i++)
+		printf("%d, ", ptr[i]);
+	printf("\n");
 	return (0);
 }
